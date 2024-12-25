13b859f6e3b33bec4e4f6b30700d43ce6f103a33
Can <99124310+canshn8@users.noreply.github.com>
2024-12-25 21:05:21 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const reqUrl = "https://portfolio-server-canshn8s-projects.vercel.app/api"; 
+export const reqUrl = "https://cansahin.vercel.app/api"; 
 
 const request = async (method, url, data) => {
   const storedUser = localStorage.getItem("persist:root");
