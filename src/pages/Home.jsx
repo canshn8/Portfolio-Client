1600391576738d8e97b0d2e25b8e6ebfeb83a173
Can <99124310+canshn8@users.noreply.github.com>
2025-02-03 16:19:12 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import CurriculumVitae from "../component/CurriculumVitae";
 import Body from "../component/Body";
 import Projects from "../component/Projects";
 import Footer from "../component/Footer";
-import CurriculumVitae from "../component/CurriculumVitae";
 
 function Home() {
   return (
