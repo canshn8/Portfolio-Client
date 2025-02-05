e8b2fea21c922ef74089dcc446a0b2242046729b
Can <99124310+canshn8@users.noreply.github.com>
2025-02-05 17:44:35 +0300

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
           </div>
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2024{" "}
+            © 2025{" "}
             <a href="/" class="hover:underline">
               Can™
             </a>
